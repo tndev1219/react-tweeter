55c48c968b3fe5a8ac649d73bb9bc61f8b1a0f0d
tndev1219 <53935661+tndev1219@users.noreply.github.com>
2020-10-22 20:46:02 +0800

    fix

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -10,5 +10,5 @@ export default {
       icon: 'en',
    },
    serverURL: 'http://3.18.60.150:4000',
-   appURL: 'https://cocky-lamport-89ea54.netlify.app'
+   appURL: 'http://18.223.110.24:81'
 };
